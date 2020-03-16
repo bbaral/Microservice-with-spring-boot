727eebdb8b800c61042e6b94175bb2b87719e369
bbaral <baralbikram@hotmail.com>
2020-03-15 23:27:44 -0400

    web security config

--- itemsharing.userservice/src/main/java/com/itemsharing/userservice/UserServiceApplication.java
+++ itemsharing.userservice/src/main/java/com/itemsharing/userservice/UserServiceApplication.java
@@ -1,12 +1,41 @@
 package com.itemsharing.userservice;
 
-
+import com.itemsharing.userservice.service.UserService;
+import com.itemsharing.userservice.service.pojo.Role;
+import com.itemsharing.userservice.service.pojo.User;
+import com.itemsharing.userservice.service.pojo.UserRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class UserServiceApplication implements CommandLineRunner {
+
+    @Autowired
+    private UserService userService;
+
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user1 = new User();
+        user1.setFirstName("John");
+        user1.setLastName("Adams");
+        user1.setUsername("jadams");
+        user1.setPassword("password");
+        user1.setEmail("jadams@gmail.com");
+
+        Set<UserRole> userRoles = new HashSet<>();
+        Role role1 = new Role();
+        role1.setRoleId(1);
+        role1.setName("ROLE_USER");
+        userRoles.add(new UserRole(user1, role1));
+
+        userService.createUser(user1);
     }
 }
